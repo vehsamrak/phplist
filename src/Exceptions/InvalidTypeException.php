b4c1c59e46e8652097c1b6a80be76923918ca509
Petr Karmashev <smonkl@bk.ru>
2018-10-15 23:16:34 +0300

    InvalidTypeException simplified

--- src/Exceptions/InvalidTypeException.php
+++ src/Exceptions/InvalidTypeException.php
@@ -3,8 +3,4 @@ namespace Vehsamrak\ListCollection\Exceptions;
 
 class InvalidTypeException extends \DomainException
 {
-    public function __construct(string $message = '')
-    {
-        parent::__construct($message);
-    }
 }
